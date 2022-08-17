e2877483572ae8fe78eb24ce0eb95fda8e29fcce
Ivan Vakulenko <johnvakulenko@gmail.com>
2022-08-17 10:00:19 +0300

    add orders and admin

--- landingpagewebside/crm/models.py
+++ landingpagewebside/crm/models.py
@@ -4,5 +4,12 @@ from django.db import models
 
 class Order(models.Model):
     order_dt = models.DateField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_name = models.CharField(max_length=200, verbose_name = 'Name')
+    order_phone = models.CharField(max_length=200, verbose_name = 'Phone')
+    
+    def __str__(self):
+        return self.order_name
+    
+class Meta:
+    verbose_name = 'Order'
+    verbose_name_plural = 'Orders'
